78bc7532cf2a54c940659b6bbf4600d4b31b37a2
lucaslp96 <lucas.maccallini@gmail.com>
2017-11-22 15:00:15 -0300

    cambios mas visitada/votada

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,8 +3,6 @@ class Question < ApplicationRecord
 	scope :porfecha, -> { order("created_at desc") }
 	scope :porvotos, -> { order("votes desc") }
 	scope :porvisitas, -> { order("visits desc") }	
-	scope :masvisitada, -> { order("visits desc").first }
-	scope :masvotada, -> { order("votes desc").first }
 
 	belongs_to :user
 	has_many :answers
@@ -12,4 +10,13 @@ class Question < ApplicationRecord
 	has_many :question_comments
 	has_many :question_votes
 	has_many :question_tags
+
+	def self.masvisitada
+		Question.all.order("visits desc").first
+	end
+
+	def self.masvotada
+		Question.all.order("votes desc").first
+	end
 end
+
