2324c68ad7c0d025fed1a5eaa3ec60a0e55a06c3
JuanMAGIC <juanman.g97@gmail.com>
2017-12-11 08:46:00 -0300

    Majeno de universidades

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -78,4 +78,53 @@ class UniversitiesController < ApplicationController
 
     end
 
+    def edit
+      @university = University.find(params[:id])
+      if ((user_signed_in?) and ((current_user.points) >= (Permit.where(action: "administrar universidades").first.required_points)))
+
+          @permitido = "true"
+
+      else
+
+          @permitido = "false"
+
+      end
+    end
+
+    def update
+      if ((user_signed_in?) and ((current_user.points) >= (Permit.where(action: "administrar universidades").first.required_points)))
+
+          @permitido = "true"
+
+      else
+
+          @permitido = "false"
+
+      end
+      @university = University.find(params[:id])
+      @university.update(params.require(:university).permit(:name))
+           if ((@university.name.length) < 3)
+              flash[:error] = "La universidad que intentó crear tenia menos de 4 caracteres."
+              render :edit
+          else
+              if ((@university.name.length) > 16)
+                flash[:error] = "La universidad que intentó crear tenia mas de 16 caracteres."
+                render :edit
+              else
+                  if (@university.save)
+                    flash[:success] = "Universidad modificada exitosamente."
+                    redirect_to universities_path
+                  else
+                    flash[:error] = "La universidad que intentó crear ya existe."  #si llego a este punto el único error que puede haber es que ya exista la etiqueta
+                    render :edit
+                  end
+              end
+          end
+        end
+    def destroy
+       @deleted_university = University.destroy(params[:id])
+       flash[:success] = "La universidad ha sido borrada."
+       redirect_to universities_path
+    end
+
 end
